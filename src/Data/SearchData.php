217d1ef42e7d8da0a80f4cc478ea3e93946bd66c
Chloé Cilia <chloe.cilia68@gmail.com>
2025-02-11 17:02:55 +0100

    add SearchData class

--- src/Data/SearchData.php
+++ src/Data/SearchData.php
@@ -4,8 +4,19 @@ namespace App\Data;
 
 class SearchData
 {
-    
-    public $page = 1;
+    // public $page = 1;
 
-    public string $q = '';
+    public string $q = ''; // champ de l'input type text 
+
+    public array $type = []; //à vérifier
+
+    public array $color= []; //à vérifier
+
+    public int $max;
+
+    public int $min;
+
 }
+
+
+// cet objet représente les données renvoyées via le formulaire de recherche et les différents champs 
