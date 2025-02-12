5a81fbd2719323075c1c97cbee2cc9377d5b9d05
Chloé Cilia <chloe.cilia68@gmail.com>
2025-02-12 09:44:13 +0100

    debug type filter

---
+++ src/Controller/Cartcontroller.php
@@ -0,0 +1,18 @@
+<?php 
+namespace App\controller;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+// #[Route('/cart', name:'cart_')]
+// class CartController extends AbstractController 
+// {   
+//     #[Route('/add/{id}', name:'add')]
+//     public function add(Product $product, SessionInterface $session)
+//     {
+
+//     }
+
+// }
--- src/Form/SearchForm.php
+++ src/Form/SearchForm.php
@@ -2,9 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Type;
 use App\Model\SearchData;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -24,21 +26,14 @@ class SearchForm extends AbstractType
                 ]
                 ])
             
-            ->add('type', ChoiceType::class, [
+            ->add('type', EntityType::class, [
+                'class' => Type::class,
+                'choice_label' => 'typeName',
                 'label' => false,
                 'required' => false,
                 'expanded' => true, //sinon on obtient une liste déroulante
                 'multiple' => false, 
                 'placeholder' => false,
-                'choices' => [
-                    'IPA' => 'ipa',
-                    'Pale Ale' => 'paleale',
-                    'DDH' => 'ddh',
-                    'NEIPA' => 'neipa',
-                    'Witbier' =>'witbier',
-                    'Pilsner' => 'pilsner',
-                    'Stout' => 'stout'
-                ],
             ])
             
             ->add('color', ChoiceType::class, [
