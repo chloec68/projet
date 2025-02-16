f71e1a1f5e4adefcf6a5b1a304e8a3b7cad697f2
Chloé Cilia <chloe.cilia68@gmail.com>
2025-02-16 21:59:20 +0100

    increase qty decrease qty add to cart ajax js

--- assets/app.js
+++ assets/app.js
@@ -53,18 +53,147 @@ import './styles/app.css';
 
   // ADD ITEM TO CART + UPDATE TOTAL ITEMS 
 
-let addButtons = document.querySelectorAll(".add-to-cart");
+// let addButtons = document.querySelectorAll(".add-to-cart");
 
-addButtons.forEach(button => {
+// addButtons.forEach(button => {
+//   button.addEventListener('click', function(){
+//     let product = button.getAttribute('data-product');
+//     let quantity = button.getAttribute('data-quantity');
+//     let inputBox = document.querySelector(`input[data-product="${product}"]`);
+//     let currentQuantity = parseInt(inputBox.value);
+
+//     inputBox.value = currentQuantity + parseInt(quantity);
+
+//     fetch('/cart/addAjax',{
+//         method : 'POST',
+//         headers :{
+//             'Content-Type':'application/json'
+//         } ,
+//         body: JSON.stringify({
+//         product:product,
+//         quantity:quantity
+//         })
+//       })
+
+//     .then(response =>
+//     {   
+//       return response.text();
+//     })
+
+//     .then(text => {
+//         try {
+//             const data = JSON.parse(text);  // Essaie de parser la réponse en JSON
+//               if(data.nbItems == 1){
+//                   document.getElementById('nbItems').textContent = data.nbItems + " article";
+//               }else{
+//                   document.getElementById('nbItems').textContent = data.nbItems + " articles";
+//               }
+//         }catch (error) {
+//             console.error('Erreur de parsing JSON:', error);
+//         }
+//     })
+
+//     .catch(error => {
+//         console.error('Erreur',error);
+//     });
+//   });
+// });
+
+
+
+  // REMOVE ITEM FROM CART + UPDATE TOTAL ITEMS 
+
+
+// let minusButtons = document.querySelectorAll(".remove-from-cart");
+
+// minusButtons.forEach(button => {
+//   button.addEventListener('click', function(){
+//     let product = button.getAttribute('data-product');
+//     let quantity = button.getAttribute('data-quantity');
+//     let inputBox = document.querySelector(`input[data-product="${product}"]`);
+//     let currentQuantity = parseInt(inputBox.value);
+
+//       if (currentQuantity - parseInt(quantity) >= 1) {
+//           inputBox.value = currentQuantity - parseInt(quantity);
+//       } else {
+//           inputBox.value = 1; 
+//       }
+
+//       fetch('/cart/removeAjax',{
+//           method : 'POST',
+//           headers :{
+//               'Content-Type':'application/json'
+//           } ,
+//           body: JSON.stringify({
+//           product:product,
+//           quantity:quantity
+//           })
+//       })
+
+//       .then(response =>
+//       {   
+//         return response.text();
+//       }) 
+
+//       .then(text => {
+//         try {
+//           const data = JSON.parse(text); 
+
+//           if(data.nbItems == 1){
+//               document.getElementById('nbItems').textContent = data.nbItems + " article";
+//           }else{
+//               document.getElementById('nbItems').textContent = data.nbItems + " articles";
+//           }
+//         }catch (error) {
+//           console.error('Erreur de parsing JSON:', error);
+//         }
+//       })
+//       .catch(error => {
+//           console.error('Erreur',error);
+//       });
+//   });
+// });
+
+
+
+// INCREMENT QUANTITY 
+
+let incrementButtons = document.querySelectorAll('.increment');
+incrementButtons.forEach(button => {
   button.addEventListener('click', function(){
     let product = button.getAttribute('data-product');
-    let quantity = button.getAttribute('data-quantity');
-    let inputBox = document.querySelector(`input[data-product="${product}"]`);
-    let currentQuantity = parseInt(inputBox.value);
+    let input = document.querySelector(`input[data-product="${product}"]`);
+    let currentQuantity = parseInt(input.value);
+   input.value = currentQuantity + 1 ; 
+  });
+}); 
 
-    inputBox.value = currentQuantity + parseInt(quantity);
+// DECREMENT QUANTITY
+let decrementButtons = document.querySelectorAll('.decrement');
 
-    fetch('/cart/addAjax',{
+decrementButtons.forEach(button => {
+  button.addEventListener('click', function(){
+    let product = button.getAttribute('data-product');
+    let input = document.querySelector(`input[data-product="${product}"]`);
+    let currentQuantity = parseInt(input.value);
+    if(currentQuantity >1) {
+      input.value = currentQuantity -1;
+    }
+  });
+});
+
+// ADD TO CART 
+
+
+let addToCartButtons = document.querySelectorAll(".add-to-cart");
+  addToCartButtons.forEach(button => {
+  button.addEventListener('click', function(){
+    let product = button.getAttribute('data-product');
+    let quantity = document.querySelector(`input[data-product="${product}"]`).value;
+
+    if(parseInt(quantity) >0){
+
+      fetch('/cart/addAjax',{
         method : 'POST',
         headers :{
             'Content-Type':'application/json'
@@ -75,81 +204,26 @@ addButtons.forEach(button => {
         })
       })
 
-    .then(response =>
-    {   
-      return response.text();
-    })
-
-    .then(text => {
-        try {
-            const data = JSON.parse(text);  // Essaie de parser la réponse en JSON
-              if(data.nbItems == 1){
-                  document.getElementById('nbItems').textContent = data.nbItems + " article";
-              }else{
-                  document.getElementById('nbItems').textContent = data.nbItems + " articles";
-              }
-        }catch (error) {
-            console.error('Erreur de parsing JSON:', error);
-        }
-    })
-
-    .catch(error => {
-        console.error('Erreur',error);
-    });
-  });
-});
-
-
-
-  // REMOVE ITEM FROM CART + UPDATE TOTAL ITEMS 
-
-
-let minusButtons = document.querySelectorAll(".remove-from-cart");
-
-minusButtons.forEach(button => {
-  button.addEventListener('click', function(){
-    let product = button.getAttribute('data-product');
-    let quantity = button.getAttribute('data-quantity');
-    let inputBox = document.querySelector(`input[data-product="${product}"]`);
-    let currentQuantity = parseInt(inputBox.value);
-
-      if (currentQuantity - parseInt(quantity) >= 1) {
-          inputBox.value = currentQuantity - parseInt(quantity);
-      } else {
-          inputBox.value = 1; 
-      }
-
-      fetch('/cart/removeAjax',{
-          method : 'POST',
-          headers :{
-              'Content-Type':'application/json'
-          } ,
-          body: JSON.stringify({
-          product:product,
-          quantity:quantity
-          })
-      })
-
       .then(response =>
-      {   
-        return response.text();
-      }) 
-
-      .then(text => {
-        try {
-          const data = JSON.parse(text); 
-
-          if(data.nbItems == 1){
-              document.getElementById('nbItems').textContent = data.nbItems + " article";
-          }else{
-              document.getElementById('nbItems').textContent = data.nbItems + " articles";
+        { return response.text();
+        })
+    
+        .then(text => {
+          try {
+              const data = JSON.parse(text);  // Essaie de parser la réponse en JSON
+                if(data.nbItems == 1){
+                    document.getElementById('nbItems').textContent = data.nbItems + " article";
+                }else{
+                    document.getElementById('nbItems').textContent = data.nbItems + " articles";
+                }
+          }catch (error) {
+              console.error('Erreur de parsing JSON:', error);
           }
-        }catch (error) {
-          console.error('Erreur de parsing JSON:', error);
-        }
-      })
-      .catch(error => {
+        })
+
+        .catch(error => {
           console.error('Erreur',error);
-      });
-  });
-});
+        });
+      }
+    })
+  })
