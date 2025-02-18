f204c07d5f0df17ab2d2ede5d8af5631af81617b
Chloé Cilia <chloe.cilia68@gmail.com>
2025-02-18 18:21:56 +0100

    cart

--- assets/app.js
+++ assets/app.js
@@ -50,9 +50,7 @@ import './styles/app.css';
   document.addEventListener('scroll', buttonVisibility);
 
 
-//*************************************************************** PRODUCT/BEERS  */
-
-// INCREMENT QUANTITY 
+// INCREMENT QUANTITY (PRODUCTS)
 
 let incrementButtons = document.querySelectorAll('.increment');
 incrementButtons.forEach(button => {
@@ -61,162 +59,146 @@ incrementButtons.forEach(button => {
     let input = document.querySelector(`input[data-product="${product}"]`);
     let currentQuantity = parseInt(input.value);
     input.value = currentQuantity + 1 ; 
+
+        updateCartTotal();
+        updateCartSubTotals();
+  });
+}); 
+
+// INCREMENT QUANTITY + ADD TO CART (CART)
+
+let incrementButtonsCart = document.querySelectorAll('.cart-increment');
+incrementButtonsCart.forEach(button => {
+  button.addEventListener('click', function(){
+    let product = button.getAttribute('cart-product');
+    let input = document.querySelector(`input[cart-product="${product}"]`);
+    let currentQuantity = parseInt(input.value);
+    input.value = currentQuantity + 1 ; 
+
+        updateCartTotal();
+        updateCartSubTotals();
+        addToCart(product, 1);
   });
 }); 
 
 // DECREMENT QUANTITY
-let decrementButtons = document.querySelectorAll('.decrement');
+let decrementButtons = document.querySelectorAll('.cart-decrement');
 
 decrementButtons.forEach(button => {
   button.addEventListener('click', function(){
-    let product = button.getAttribute('data-product');
-    let input = document.querySelector(`input[data-product="${product}"]`);
+    let product = button.getAttribute('cart-product');
+    let input = document.querySelector(`input[cart-product="${product}"]`);
     let currentQuantity = parseInt(input.value);
-    if(currentQuantity >1) {
+    if(currentQuantity > 0) {
       input.value = currentQuantity -1;
+      updateCartTotals()
+      updateCartSubTotals()
     }
   });
 });
 
-// ADD TO CART 
+// ADD TO CART FROM PRODUCTS PAGE VIA ADD TO CART BUTTON
 let addToCartButtons = document.querySelectorAll(".add-to-cart");
-  addToCartButtons.forEach(button => {
-  button.addEventListener('click', function(){
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', function() {
     let product = button.getAttribute('data-product');
     let quantity = document.querySelector(`input[data-product="${product}"]`).value;
 
-    if(parseInt(quantity) >0){
+    if (parseInt(quantity) > 0) {
+      addToCart(product, quantity);
+    }
+  });
+});
 
-      fetch('/cart/addAjax',{
-        method : 'POST',
-        headers :{
-            'Content-Type':'application/json'
-        } ,
-        body: JSON.stringify({
-        product:product,
-        quantity:quantity
-        })
-      })
+// UPDATE CART TOTALS (NB ITEMS + TOTAL PRICE)
+function updateCartTotal(){
 
-      .then(response => response.json())
-    
-        .then(data => {
-            if(data.nbItems == 1){
-                document.getElementById('nbItems').textContent = data.nbItems + " article";
-                alert('produit ajouté au panier');
-            }else{
-                document.getElementById('nbItems').textContent = data.nbItems + " articles";
-                alert('produit ajouté au panier');
-            }
-          })
-        .catch(error => {
-          console.error('Erreur',error);
-        });
-      }
+  let totalPrice = 0; 
+  let cartItems = document.querySelectorAll('.cart-item');
+  cartItems.forEach(item => {
+    let input = item.querySelector('input[cart-product]');
+    let quantity = parseInt(input.value);
+    let price = parseFloat(item.querySelector('.product-price').textContent);
+    totalPrice += price*quantity;
+    document.querySelector('.total-price').textContent = totalPrice.toFixed(2) + " €";
+  })
+}
+
+// UPDATE CART SUBTOTALS (NB ITEMS + TOTAL PRICE)
+function updateCartSubTotals(){
+  let cartItems = document.querySelectorAll('.cart-item');
+  let subTotal = 0; 
+  cartItems.forEach(item => {
+    let quantity = item.querySelector('input[type="number"].input-box').value;
+    let itemPriceElement = item.querySelector('.product-price');
+    let itemPrice = parseFloat(itemPriceElement.textContent);
+    subTotal = itemPrice * quantity 
+    let subTotalElement = item.querySelector('.sub-total');
+    subTotalElement.textContent = subTotal.toFixed(2) + " €";
+  })
+}
+
+
+// ADD TO CART 
+function addToCart(product,quantity){
+
+  fetch('/cart/add/{id}',{
+    method : 'POST',
+    headers :{
+        'Content-Type':'application/json'
+    } ,
+    body: JSON.stringify({
+    product:product,
+    quantity:quantity
     })
   })
 
-  //*************************************************************** CART  */
+  .then(response => response.json())
 
-  // ADD ITEM TO CART + UPDATE TOTAL ITEMS 
-
-// let addButtons = document.querySelectorAll(".add");
-
-// addButtons.forEach(button => {
-//   button.addEventListener('click', function(){
-//     let product = button.getAttribute('data-product');
-//     let quantity = button.getAttribute('data-quantity');
-//     let inputBox = document.querySelector(`input[data-product="${product}"]`);
-//     let currentQuantity = parseInt(inputBox.value);
-
-//     inputBox.value = currentQuantity + parseInt(quantity);
-
-//     fetch('/cart/addAjax',{
-//         method : 'POST',
-//         headers :{
-//             'Content-Type':'application/json'
-//         } ,
-//         body: JSON.stringify({
-//         product:product,
-//         quantity:quantity
-//         })
-//       })
-
-//     .then(response =>
-//     {   
-//       return response.text();
-//     })
-
-//     .then(text => {
-//         try {
-//             const data = JSON.parse(text);  // Essaie de parser la réponse en JSON
-//               if(data.nbItems == 1){
-//                   document.getElementById('nbItems').textContent = data.nbItems + " article";
-//               }else{
-//                   document.getElementById('nbItems').textContent = data.nbItems + " articles";
-//               }
-//         }catch (error) {
-//             console.error('Erreur de parsing JSON:', error);
-//         }
-//     })
-
-//     .catch(error => {
-//         console.error('Erreur',error);
-//     });
-//   });
-// });
+    .then(data => {
+        if(data.nbItems == 1){
+            document.querySelector('.nbItems').textContent = data.nbItems + " article";
+            alert('produit ajouté au panier');
+        }else{
+            document.querySelector('.nbItems').textContent = data.nbItems + " articles";
+            alert('produit ajouté au panier');
+        }
+      })
+    .catch(error => {
+      console.error('Erreur',error);
+    });
+  }
 
 
+ // DELETE ITEM FROM CART 
 
-  // REMOVE ITEM FROM CART + UPDATE TOTAL ITEMS 
+  let removeItemButton = document.querySelectorAll('.remove-item');
 
+  removeItemButton.forEach(button => {
+    button.addEventListener('click',function(){
+      let product = button.getAttribute('cart-product');
+      let url = `/cart/remove/${product}`;
 
-// let minusButtons = document.querySelectorAll(".remove-from-cart");
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type':'application/json',
+        },
+      })
+      .then(response=>response.json())
+      .then(data => {
+        if(data.success){
+          let productTable = document.querySelector(`table[cart-item="${product}"]`);
+          if (productTable){
+            productTable.remove();
+          }
+        }else{
+          console.error('erreur lors de la suppression'); // à modifier ultérieurement !!!!!!!!!!!!!!!
+        }
+      })
+      .catch(error => console.error('Erreur:', error));
+    });
+  });
 
-// minusButtons.forEach(button => {
-//   button.addEventListener('click', function(){
-//     let product = button.getAttribute('data-product');
-//     let quantity = button.getAttribute('data-quantity');
-//     let inputBox = document.querySelector(`input[data-product="${product}"]`);
-//     let currentQuantity = parseInt(inputBox.value);
+ 
 
-//       if (currentQuantity - parseInt(quantity) >= 1) {
-//           inputBox.value = currentQuantity - parseInt(quantity);
-//       } else {
-//           inputBox.value = 1; 
-//       }
-
-//       fetch('/cart/removeAjax',{
-//           method : 'POST',
-//           headers :{
-//               'Content-Type':'application/json'
-//           } ,
-//           body: JSON.stringify({
-//           product:product,
-//           quantity:quantity
-//           })
-//       })
-
-//       .then(response =>
-//       {   
-//         return response.text();
-//       }) 
-
-//       .then(text => {
-//         try {
-//           const data = JSON.parse(text); 
-
-//           if(data.nbItems == 1){
-//               document.getElementById('nbItems').textContent = data.nbItems + " article";
-//           }else{
-//               document.getElementById('nbItems').textContent = data.nbItems + " articles";
-//           }
-//         }catch (error) {
-//           console.error('Erreur de parsing JSON:', error);
-//         }
-//       })
-//       .catch(error => {
-//           console.error('Erreur',error);
-//       });
-//   });
-// });
--- src/Controller/Cartcontroller.php
+++ src/Controller/Cartcontroller.php
@@ -23,6 +23,7 @@ class CartController extends AbstractController
         $total=0;
         $subtotal=0;
         $nbItems=0;
+        $quantity = 0;
   
         foreach($cart as $id=>$quantity){
             $product = $productRepository->find($id);
@@ -40,130 +41,54 @@ class CartController extends AbstractController
 
         return $this->render('cart/index.html.twig',[       
             'data'=>$data,
-
             'total'=>$total,
             'subtotal'=>$subtotal,
-            'nbItems'=>$nbItems
+            'nbItems'=>$nbItems,
+            'quantity'=>$quantity
             ]);
     }
 
-    #[Route('cart/add/{id}', name:'app_cart-add')]
-    public function add(Product $product, SessionInterface $session)
-    {   
-        //on récupère l'id du produit 
-        $id = $product->getId();
+       // ADD ITEM TO CART  
 
-        //je récupère le panier existant s'il y en a un - si panier n'existe pas en session, on en crée un (tableau vide) ;
-        $cart = $session->get('cart',[]);
+       #[Route('/cart/add/{id}', name:'app_cart-add')]
+       public function add(Request $request, SessionInterface $session)
+       {
+           $data = json_decode($request->getContent(),true);
+   
+           $product = $data['product'];
+           $quantity = $data['quantity']; 
+           $cart = $session->get('cart',[]);
+   
+           if(empty($cart[$product])){
+               $cart[$product] = $quantity;
+           }else{
+               $cart[$product] += $quantity; 
+           }
+   
+           $session->set('cart',$cart);
+   
+           $nbItems = array_sum($cart); // array_sum() => native PHP function that returns the sum of values in an array
+   
+           return new JsonResponse(['success' => true,'nbItems'=>$nbItems,'cart' => $cart]);
+       }
 
-        //on ajoute le produit dans le panier s'il n'y est pas encore - sinon on incrémente la qté
-        if(empty($cart[$id])){
-            $cart[$id] =1;
-        }else{
-            $cart[$id]++; 
-        }
-       
-       //j'ajoute le panier à la session en lui attribuant la clé (le nom) 'panier'
-       $session->set('cart',$cart);
-
-    //    dd($session);
-
-        return $this->redirectToRoute('app_cart');
-    }
-
-    #[Route('cart/empty', name:'app_cart-empty')]
-    public function emptyCart(SessionInterface $session)
-    {
-        $cart = $session->get('cart',[]); 
-        if($cart){
-            $session->unset('cart',$cart);
-        }else{
-            $message = "Le panier est déjà vide";
-        }
-
-        return $this->render('cart/index.html.twig',[
-            "message" => $message
-        ]);
-    }
-
+    // REMOVE AN ITEM FROM CART 
     #[Route('cart/remove/{id}', name:'app_cart-remove')]
-    public function removeProduct(SessionInterface $session,Product $product)
-    {
+    public function removeProduct(SessionInterface $session,Product $product, Request $request)
+    {   
+        $data = json_decode($request->getContent(),true);
         $cart = $session->get('cart',[]);
         $id = $product->getId();
 
         if(!empty($cart[$id])){
-            if($cart[$id] > 1){
-                $cart[$id]--;
-            }else{
-                unset($cart[$id]);
-            }
+            unset($cart[$id]);
         }
-
-        $session->set('cart',$cart);
-
-        return $this->redirectToRoute('app_cart');
+        
+    $session->set('cart',$cart);
+    $nbItems = array_sum($cart);
+        
+    return new JsonResponse(['success' => true, 'nbItems'=>$nbItems]);
     }
+    
 
-    // ***********************************************************************************************************
-
-    // ADD ITEM FROM BASKET 
-
-    #[Route('/cart/addAjax', name:'app_cart-addAjax')]
-    public function addAjax(Request $request, SessionInterface $session)
-    {
-        //json_decode() => fonction native PHP qui récupère une chaîne encodée JSON et la convertit en valeur PHP
-        // paramètre 'true' => les objets JSON sont retournés comme tableaux associatifs
-            // {
-            //     "productId":1,
-            //     "quantity":2
-            // }
-            // devient 
-            // ["productId" => 1,
-            // "quantity" => 2]
-        // paramètre 'false' => les objets JSON sont retournés comme des objets 
-        $data = json_decode($request->getContent(),true);
-
-        $product = $data['product'];
-        $quantity = $data['quantity']; 
-        $cart = $session->get('cart',[]);
-
-        if(empty($cart[$product])){
-            $cart[$product] = $quantity;
-        }else{
-            $cart[$product] += $quantity; 
-        }
-
-        $session->set('cart',$cart);
-
-        $nbItems = array_sum($cart); // array_sum() => native PHP function that returns the sum of values in an array
-
-        return new JsonResponse(['nbItems'=>$nbItems]);
-    }
-
-    // REMOVE ITEM FROM BASKET 
-
-    #[Route('/cart/removeAjax', name:'app_cart-removeAjax')]
-    public function removeAjax(Request $request, SessionInterface $session)
-    { 
-        $data = json_decode($request->getContent(),true);
-
-        $product = $data['product'];
-        $quantity = $data['quantity']; 
-        $cart = $session->get('cart',[]);
-
-        if(isset($cart[$product])){
-            if($cart[$product] >1){
-                $cart[$product] --;
-            }else{
-                unset ($cart[$product]); 
-            }
-            $session->set('cart',$cart);
-            $nbItems = array_sum($cart);
-
-            return new JsonResponse(['nbItems'=>$nbItems]);
-        }else{
-            return new JsonResponse(['error' => 'Produit non trouvé dans le panier'], 400);
-        }
-    }
 }
