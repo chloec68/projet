1d84a9b33f880a2036d6cc6e80793ce74dbfa933
Chloé Cilia <chloe.cilia68@gmail.com>
2025-02-23 13:17:44 +0100

    fix mistake

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -5,9 +5,9 @@ namespace App\Controller;
 use Stripe\Stripe;
 use Stripe\Checkout\Session;
 use App\Entity\Establishment;
-use Doctrine\ORM\EntityManager;
 use App\Form\CollectionFormType;
 use App\Form\IdentificationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\EstablishmentRepository;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
@@ -35,7 +35,7 @@ class PaymentController extends AbstractController
             //enregistrement en session
             $session->set('identificationData',$data);
             // redirection vers la page choisir point de retrait
-            dd($session);
+            // dd($session);
             return $this->redirectToRoute('app_payment-collection');
         }
 
@@ -96,65 +96,65 @@ class PaymentController extends AbstractController
                     'price_data' => [
                         'currency' => 'eur',
                         'product_data' => [
-                            'name' => 'Commande',  // Nom générique ou autre
+                            'name' => 'Commande',  // Nom générique
                         ],
-                        'unit_amount' => $priceTotal * 100,  // Montant en cents (exemple pour 5.00 USD => 500)
+                        'unit_amount' => $priceTotal * 100,  // Montant en cents (exemple pour 5.00 EUR => 500)
                     ],
-                    'quantity' => 1,  // Tu peux juste mettre une quantité de 1 si tu passes un seul montant
+                    'quantity' => 1,  // quantité de 1 car un seul montant (total)
                 ],
             ],
             'customer_email'=> $userEmail,
             'mode' => 'payment',
-            'success_url' => $YOUR_DOMAIN . '/success.html.twig',
-            'cancel_url' => $YOUR_DOMAIN . '/cancel.html.twig',
+            'success_url' => $YOUR_DOMAIN . '/payment/checkout/success',
+            'cancel_url' => $YOUR_DOMAIN . '/payment/checkout/cancel',
             ]);
             return new RedirectResponse($checkout_session->url);
         }
 
-        #[Route('/payment/checkout/success', name:'app_payment-checkout_success')]
-        public function success(SessionInterface $session, Security $security, EntityManager $entityManager):Response
+        #[Route('/payment/checkout/success', name:'app_payment-checkout-success')]
+        public function success(SessionInterface $session, Security $security, EntityManagerInterface $entityManager):Response
         {
-            $order = new Order;
-            $order->setOrderDateOfPlacement(now());
-            $order->setOrderReference(uniqid('stecru-e'));
+            // $order = new Order;
+            // $order->setOrderDateOfPlacement(now());
+            // $order->setOrderReference(uniqid('stecru-e'));
 
-            $userFirstName = $session->get('orderUserFirstName');
-            $order->setOrderUserFirstName($userFirstName);
+            // $userFirstName = $session->get('orderUserFirstName');
+            // $order->setOrderUserFirstName($userFirstName);
 
-            $userLastName = $session->get('orderUserLastName');
-            $order->setOrderUserLastName($userLastName);
+            // $userLastName = $session->get('orderUserLastName');
+            // $order->setOrderUserLastName($userLastName);
 
-            $email = $session->get('orderEmail');
-            $order->setOrderEmail($email);
+            // $email = $session->get('orderEmail');
+            // $order->setOrderEmail($email);
 
-            $cart = $session->get('cart');
-            $priceTotal= $cart['priceTotal'];
-            $order->setOrderTotal($priceTotal);
+            // $cart = $session->get('cart');
+            // $priceTotal= $cart['priceTotal'];
+            // $order->setOrderTotal($priceTotal);
 
-            foreach ($cart as $product => $quantity) {
-                $order->add($product);
-            }
-            $establishment = $session->get('establishment');
-            $order->setEstablishment($establishment);
+            // foreach ($cart as $product => $quantity) {
+            //     $order->add($product);
+            // }
+            // $establishment = $session->get('establishment');
+            // $order->setEstablishment($establishment);
 
-            $appUser = $security->getUser();
+            // $appUser = $security->getUser();
 
-            if(isset($user)){
-                $order->setAppUser($appUser);
-            }
+            // if(isset($user)){
+            //     $order->setAppUser($appUser);
+            // }
 
-            $entityManager->persist($order);
-            $entityManager->flush();
+            // $entityManager->persist($order);
+            // $entityManager->flush();
 
             return $this->render('/payment/success.html.twig', [
                 'controller_name' => 'Paymentcontroller',
             ]);
          }
 
-        #[Route('/payment/checkout/success', name:'app_payment-checkout_error')]
+        #[Route('/payment/checkout/cancel', name:'app_payment-checkout-cancel')]
         public function error(SessionInterface $session):Response
         { 
-            return $this->render('/payment/error.html.twig', [
+            return $this->render('/payment/cancel.html.twig', [
                 'controller_name' => 'Paymentcontroller',
             ]);
         }
