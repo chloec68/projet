6aa458654f63f1e3e479bc7ba436560376a41202
Chloé Cilia <chloe.cilia68@gmail.com>
2025-03-04 17:00:40 +0100

    user profile view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -19,11 +20,20 @@ final class HomeController extends AbstractController
         $ephemeral = $productRepository->findByPermanency(false,1);
 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
             'permanent' => $permanent,
             'ephemeral' => $ephemeral,
             'meta_description' => 'Bienvenue sur le site de la Brasserie locale et artisanale Sainte Cru sur laquelle vous pourrez découvrir nos produits et les acheter directement en ligne'
         ]);
     }
 
+    #[Route('/user', name: 'app_user')]
+    public function userProfile(Security $security):Response
+    {   
+        $user = $security->getUser();
+
+        return $this->render('home/profile.html.twig', [
+            'user' => $user,
+            'meta_description' => ''
+        ]);
+    }
 }
