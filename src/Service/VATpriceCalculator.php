066a8afee962a62ff99a223505785423c6500fcc
Chloé Cilia <chloe.cilia68@gmail.com>
2025-03-05 17:09:03 +0100

    add method in VATpriceCalculator service

--- src/Service/VATpriceCalculator.php
+++ src/Service/VATpriceCalculator.php
@@ -12,5 +12,17 @@ class VATpriceCalculator{
 
         return number_format($result,2,'.','');
     }
+
+    public function vatPriceSubTotal(Product $product, int $quantity) : float 
+    { 
+        $price = $product->getProductPrice() ; 
+        $vatRate = $product->getVat()->getVatRate();
+        $vat = $price * $vatRate ; 
+        $vatPrice = $price + $vat ; 
+        $subTotal = $vatPrice * $quantity;
+
+        return number_format($subTotal,2,'.',' ') ;
+    }
 }
+
 
