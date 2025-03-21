fdc534722c86ff077a709887ab20f8c57ea9e8aa
Chloé Cilia <chloe.cilia68@gmail.com>
2025-03-21 22:37:53 +0100

    add admin redirection

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -48,6 +48,11 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
+        $user = $token->getUser();
+        if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+            return new RedirectResponse($this->urlGenerator->generate('admin'));
+        }
+
         return new RedirectResponse($this->urlGenerator->generate('app_home'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
