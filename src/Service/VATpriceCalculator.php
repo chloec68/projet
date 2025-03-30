3df7b3219007bf626b2cf06ec9e7bc7d9e395464
Chloé Cilia <chloe.cilia68@gmail.com>
2025-03-30 13:40:21 +0200

    add comments

--- src/Service/VATpriceCalculator.php
+++ src/Service/VATpriceCalculator.php
@@ -7,18 +7,17 @@ use App\Entity\Product;
 class VATpriceCalculator{
 
     public function vatPrice(Product $product): float
-    {
-        $result = $product->getProductPrice() * (1 + $product->getVAT()->getVATRate());
-
-        return number_format($result,2,'.','');
+    {   
+         // récupère le prix de base (H.T.) du produit et le multiplie par (1 + taux de TVA)
+        $vatPrice= $product->getProductPrice() * (1 + $product->getVAT()->getVATRate());
+        // retourne le prix T.T.C., formaté avec deux décimales (pour l'affichage)
+        return number_format($vatPrice,2,'.','');
     }
 
     public function vatPriceSubTotal(Product $product, int $quantity) : float 
-    { 
-        $price = $product->getProductPrice() ; 
-        $vatRate = $product->getVat()->getVatRate();
-        $vat = $price * $vatRate ; 
-        $vatPrice = $price + $vat ; 
+    {   
+        $vatPrice= $product->getProductPrice() * (1 + $product->getVAT()->getVATRate());
+
         $subTotal = $vatPrice * $quantity;
 
         return number_format($subTotal,2,'.',' ') ;
@@ -26,3 +25,14 @@ class VATpriceCalculator{
 }
 
 
+        // $price = $product->getProductPrice() ; 
+      
+        // $vatRate = $product->getVat()->getVatRate();
+     
+        // $vat = $price * $vatRate ; 
+    
+        // $vatPrice = $price + $vat ; 
+
+        // $subTotal = $vatPrice * $quantity;
+
+        // return number_format($subTotal,2,'.',' ') ;
