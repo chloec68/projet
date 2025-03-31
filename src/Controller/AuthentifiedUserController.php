3fa6e5d3be124a66723f74176460ef50618fca98
Chloé Cilia <chloe.cilia68@gmail.com>
2025-03-31 10:33:23 +0200

    update delete account method

--- src/Controller/AuthentifiedUserController.php
+++ src/Controller/AuthentifiedUserController.php
@@ -112,13 +112,12 @@ final class AuthentifiedUserController extends AbstractController
             $email = $user->getEmail(); // je récupère l'email de l'utilisateur 
             $salt = bin2hex(random_bytes(16)); // je génère un sel pour éviter d'obtenir un hache identique
             $hashedEmail = hash('sha256',$email . $salt); // je hache l'adresse email 
-            $password = $user->getPassword(); // je récupère le mot de passe 
-            $rehashedPassword = password_hash(bin2hex(random_bytes(16)),PASSWORD_DEFAULT); // je re-hache le mot de passe
             $user->setEmail($hashedEmail); // je remplace l'adresse email par l'adresse hachée et donc anonymisée
+            $newPassword = password_hash(bin2hex(random_bytes(16)),PASSWORD_DEFAULT); // je crée un nouveau mot de passe
+            $user->setPassword($newPassword);
             $user->setRoles(["ROLE_DELETED"]); // je remplace le rôle initial par "rôle supprimé"
             $entityManager->persist($user); // je persiste l'utilisateur 
             $entityManager->flush(); // j'enregistre le changement en base de données 
-            // $session = new Session(); 
             $session->invalidate(); // j'invalide la session 
             $this->addFlash('success', 'Votre compte a été supprimé avec succès'); // message de succès
             return $this->redirectToRoute('app_home'); // redirection vers page d'accueil
