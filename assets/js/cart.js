ecf87d2c19cbf7b33a0eee0755e1fd6b0824085f
Chloé Cilia <chloe.cilia68@gmail.com>
2025-04-04 22:31:17 +0200

    cleaning

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -38,43 +38,6 @@ const successMsg = document.getElementById("success-msg");
 const addToCartButtons = document.querySelectorAll(".add-to-cart");
 const stockElements = document.querySelectorAll('.stock-info');
 
-// stockElements.forEach(element => {
-//   let stockQuantity = parseInt(element.getAttribute('data-stock'));
-//     if (stockQuantity > 10){
-//       if(addToCartButtons){
-//         addToCartButtons.forEach(button => {
-//           button.addEventListener('click', function() {
-//             let product = button.getAttribute('data-product');
-//             let quantity = document.querySelector(`input[data-product="${product}"]`).value;
-        
-//             if(alertBox && alertMessage && closeAlert){
-//               if(quantity > 1){
-//                 alertMessage.innerHTML = "Produits ajoutés au panier";
-//                 alertBox.style.display = "block";
-//               }else if(quantity == 1) {
-//                 alertMessage.innerHTML = "Produit ajouté au panier";
-//                 alertBox.style.display = "block";
-//               }else{
-//                 alertBox.style.display = "none";
-//               }
-          
-//               closeAlert.addEventListener('click',function(){
-//                 alertBox.style.display = "none";
-//               })
-//             }
-//             if (parseInt(quantity) > 0) {
-//               updateCart(product,quantity); 
-//             }
-//           });
-//         });
-//       }
-//   }else{
-//     addToCartButtons.forEach(button => {
-//         button.disabled = true;
-//     })
-//   }
-// });
-
 stockElements.forEach(element => {
   let stockQuantity = parseInt(element.getAttribute('data-stock'));
   let product = element.getAttribute('data-product'); // récupère le produit associé à l'élément de stock
